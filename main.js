14b46e0949bdeef765f52c803ddb0cb12abe4bad
Rayhanma <67584512+Mr22XX@users.noreply.github.com>
2023-10-08 05:38:26 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,169 @@
+let hH = document.querySelector('.hH')
+let dS = document.querySelector('.dS')
+let aH = document.querySelector('.aH')
+let gambar = document.querySelector('.gambar')
+let content2 = document.querySelector('.content2')
+let content3 = document.querySelector('.content3')
+let content4 = document.querySelector('.content4')
+let nav = document.querySelector('.nav')
+let audio = document.querySelector('.salammp3')
+let audio1 = document.querySelector('.alif')
+let audio2 = document.querySelector('.ba')
+let audio3 = document.querySelector('.ta')
+let audio4 = document.querySelector('.tsa')
+let audio5 = document.querySelector('.jim')
+let audio6 = document.querySelector('.kha')
+let audio7 = document.querySelector('.kho')
+let audio8 = document.querySelector('.dal')
+let audio9 = document.querySelector('.dzal')
+let audio10 = document.querySelector('.ro')
+let audio11 = document.querySelector('.zay')
+let audio12 = document.querySelector('.sin')
+let audio13 = document.querySelector('.syin')
+let audio14 = document.querySelector('.shod')
+let audio15 = document.querySelector('.dhod')
+let audio16 = document.querySelector('.tho')
+let audio17 = document.querySelector('.zho')
+let audio18 = document.querySelector('.ain')
+let audio19 = document.querySelector('.ghoin')
+let audio20 = document.querySelector('.fa')
+let audio21 = document.querySelector('.qof')
+let audio22 = document.querySelector('.kaf')
+let audio23 = document.querySelector('.lam')
+let audio24 = document.querySelector('.mim')
+let audio25 = document.querySelector('.nun')
+let audio26 = document.querySelector('.waw')
+let audio27 = document.querySelector('.ha')
+let audio28 = document.querySelector('.la')
+let audio29 = document.querySelector('.hamzah')
+let audio30 = document.querySelector('.ya')
+
+function hurufH(){
+    hH.style.display ="none"
+    dS.style.display ="none"
+    aH.style.display ="none"
+    content2.style.display = 'block'
+    content4.style.display = 'none'
+}
+function hurufdS(){
+    hH.style.display ="none"
+    dS.style.display ="none"
+    aH.style.display ="none"
+    content3.style.display = 'block'
+    content4.style.display = 'none'
+   
+}
+function hurufaH(){
+   alert('this part on progress')
+   content4.style.display = 'none'
+    
+}
+
+function back(){
+    hH.style.display ="block"
+    dS.style.display ="block"
+    aH.style.display ="block"
+    content2.style.display = 'none'
+    content4.style.display = 'none'
+}
+
+function alfatihah(){
+    content3.style.display = 'none'
+    gambar.style.display = 'none'
+    content4.style.display = 'block'
+}
+
+function salam(){
+    audio.play()
+}
+
+function alif(){
+    audio1.play()
+}
+function ba(){
+    audio2.play()
+}
+function ta(){
+    audio3.play()
+}
+function tsa(){
+    audio4.play()
+}
+function jim(){
+    audio5.play()
+}
+function kha(){
+    audio6.play()
+}
+function kho(){
+    audio7.play()
+}
+function dal(){
+    audio8.play()
+}
+function dzal(){
+    audio9.play()
+}
+function ro(){
+    audio10.play()
+}
+function zay(){
+    audio11.play()
+}
+function sin(){
+    audio12.play()
+}
+function syin(){
+    audio13.play()
+}
+function shod(){
+    audio14.play()
+}
+function dhod(){
+    audio15.play()
+}
+function tho(){
+    audio16.play()
+}
+function zho(){
+    audio17.play()
+}
+function ain(){
+    audio18.play()
+}
+function ghoin(){
+    audio19.play()
+}
+function fa(){
+    audio20.play()
+}
+function qof(){
+    audio21.play()
+}
+function kaf(){
+    audio22.play()
+}
+function lam(){
+    audio23.play()
+}
+function mim(){
+    audio24.play()
+}
+function nun(){
+    audio25.play()
+}
+function waw(){
+    audio26.play()
+}
+function ha(){
+    audio27.play()
+}
+function la(){
+    audio28.play()
+}
+function hamzah(){
+    audio29.play()
+}
+function ya(){
+    audio30.play()
+}
